1b957279a591f648655819af4aca0ccf8b53461d
dalai-s <62624489+Dalai-S@users.noreply.github.com>
2024-02-12 22:58:28 +0000

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -12,54 +12,64 @@ export default function Page() {
 
   const getNoButtonText = () => {
     const phrases = [
-      "No",
-      "Are you sure?",
-      "Really sure?",
-      "Think again!",
-      "Last chance!",
-      "Surely not?",
-      "You might regret this!",
-      "Give it another thought!",
-      "Are you absolutely certain?",
-      "This could be a mistake!",
-      "Have a heart!",
-      "Don't be so cold!",
-      "Change of heart?",
-      "Wouldn't you reconsider?",
-      "Is that your final answer?",
-      "You're breaking my heart ;(",
+      "Точно-преточно нет?",
+      "А ну-ка, подумай еще разок!",
+      "Серьёзно так решила?",
+      "Эй, не спеши с ответом!",
+      "Ты же знаешь, это последний шансик!",
+      "Вот уж не ожидал от тебя такого, а?",
+      "Вдруг пожалеешь – и что тогда?",
+      "Давай, перебери варианты еще раз!",
+      "Ты уверена на все сто пудов?",
+      "Ой, кажется, кто-то собирается на лёд тонкий!",
+      "Будь добрее, а?",
+      "Так, стоп! Разморозь сердечко!",
+      "А вдруг захочешь обратно? Передумай!",
+      "Эх, не хочешь ли ты это обдумать заново?",
+      "Это твой последний слово, да?",
+      "Ай-яй-яй, моё сердце на кусочки ;(",
+      "Сто процентов уверен? На все сто?",
+      "Ты на краю пропасти, думай!",
+      "Да будь милее, ну!",
+      "Остановись, дай сердцу голос!",
+      "Может, все же передумаешь?",
+      "Перед тем как прыгнуть, посмотри!",
+      "Так, ну это твоё слово последнее?",
+      "Ох, и давай мне тогда платок (((",
     ];
 
     return phrases[Math.min(noCount, phrases.length - 1)];
   };
 
   return (
-    <div className="centered-container">      
+    <div className="centered-container">
       <div className="valentine-container">
         {yesPressed ? (
           <>
-            <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
-            <div className="text-container">Ok yay!!!</div>
+            <img src="https://media1.tenor.com/m/jjM8wEXXNqwAAAAd/kiss.gif" />
+            <div className="text-container">Ну спасибочки !!! :3 </div>
           </>
         ) : (
           <>
             <img
               className="h-[200px]"
               style={{ width: "400x", height: "240px" }}
-              src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
+              src="https://media1.tenor.com/m/knRCenmW3yMAAAAC/eid.gif"
             />
-            <h1 className="text-container">Will you be my Valentine?</h1>
+            <h1 className="text-container">
+              Карина, будешь ли ты моей валентинкой?
+            </h1>
             <div>
               <button
                 className={"yes-button"}
                 style={{ fontSize: yesButtonSize }}
                 onClick={() => setYesPressed(true)}
               >
-                Yes
+                Да
               </button>
 
               <button onClick={handleNoClick} className="no-button">
-                {noCount === 0 ? "No" : getNoButtonText()}
+                {noCount === 0 ? "Нет" : getNoButtonText()}
               </button>
             </div>
           </>
